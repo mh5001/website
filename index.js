56a1fda14e3b518d78ae326acfef2e550f8ed0ab
Unknown <lordminhhuy5000@gmail.com>
2017-12-04 01:28:50 +0200

    test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var static = require('node-static');
 var file = new static.Server();
 
 require('http').createServer(function (req, res) {
+  console.log(req.headers.host);
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(req.url);
   res.end();
